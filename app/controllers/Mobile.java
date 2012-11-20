6aa1de0c0c9e254ee25c9646156ec0dce6690766
Ryunhee Han <ryuneeee@gmail.com>
2012-11-21 02:01:10 +0900

    Modify 'me' Controller
    Signed-off-by: Ryunhee Han <ryuneeee@gmail.com>

--- app/controllers/Mobile.java
+++ app/controllers/Mobile.java
@@ -1,12 +1,19 @@
 package controllers;
 
 
+import java.util.List;
+
 import models.Look;
+import models.User;
 import models.UserLook;
-import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.*;
+import views.html.mobileCoupon;
+import views.html.mobileFacebook;
+import views.html.mobileIndex;
+import views.html.mobileMe;
+import views.html.mobileOtherLook;
+import views.html.mobileProduct;
 
 
 public class Mobile extends Controller {
@@ -31,6 +38,12 @@ public class Mobile extends Controller {
 	public static Result me() {
 		//UserLook userLook = UserLook.find.byId(new Long(9));
 		//userLook.get
+		
+//		String hash = null;
+//		UserLook userLook = UserLook.find.where().ilike("imageFileName", hash).findUnique();
+//		User user = userLook.getUser();
+//		List<UserLook> userLooks = user.getUserLooks();
+//		Route나 POST hash 받고, userLooks 넘기면됨.
 		return ok(mobileMe.render());
 	}
 	
